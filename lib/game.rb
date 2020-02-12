add0253c77121b3859ecc06d4decd553ec5cb586
Julian George <jules104@gmail.com>
2020-02-12 15:18:25 +0000

    Calculate bonus points for second frame

--- lib/game.rb
+++ lib/game.rb
@@ -2,22 +2,30 @@
 
 # game class
 class Game
-
   def initialize
     @game_score = []
+    @bonus_score = []
   end
 
   def roll(int)
-   @game_score << int
-   int
+    @game_score << int
+    if @game_score.count == 3
+      calculate_bonus
+    end
+    int
   end
 
   def current_score
-   @game_score.sum
+    @game_score.sum + @bonus_score.sum
   end
 
-  def current_frame
-    frame = @game_score.length/2.to_f
-    frame = frame.round()
+  def calculate_bonus
+    game_score = @game_score
+    roll_one = game_score[0].to_i
+    roll_two = game_score[1].to_i
+       if roll_one + roll_two == 10
+          bonus = game_score[2].to_i
+          @bonus_score << bonus
+       end
   end
 end
